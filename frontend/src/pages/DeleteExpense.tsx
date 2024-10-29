8c9a5fea0665428e86354dd8f6c8567731f26f1d
Varun MK <varunmk@Varuns-MacBook-Air-2.local>
2024-10-29 12:43:30 -0400

    Added Edit Expense and Delete Expense Pages

---
+++ frontend/src/pages/DeleteExpense.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  Center,
+  Container,
+  createListCollection,
+  Flex,
+  Heading,
+  Icon,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "../components/ui/select";
+import { Button } from "../components/ui/button";
+import { useForm } from "react-hook-form";
+import { FaDollarSign } from "react-icons/fa";
+import { FaEuroSign } from "react-icons/fa";
+import { FaIndianRupeeSign } from "react-icons/fa6";
+import {
+  NumberInputField,
+  NumberInputRoot,
+} from "../components/ui/number-input";
+
+const DeleteExpense = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  function onSubmit(values: any) {
+    console.log("Hi");
+  }
+  return (
+    <Container>
+      <Center>
+        <Heading fontWeight="bold" marginBottom="10px">
+          Delete Expenses
+        </Heading>
+      </Center>
+      <Text fontSize="sm" marginBottom="10px" fontWeight="bold" color="teal">
+        Select all the transactions that need to be removed.
+      </Text>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Button mt={4} colorPalette="red" loading={isSubmitting} type="submit">
+          Delete
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+const frameworks = createListCollection({
+  items: [
+    {
+      label: (
+        <Icon>
+          <FaDollarSign />
+        </Icon>
+      ),
+      value: "dollar",
+    },
+    {
+      label: (
+        <Icon>
+          <FaEuroSign />
+        </Icon>
+      ),
+      value: "euro",
+    },
+    {
+      label: (
+        <Icon>
+          <FaIndianRupeeSign />
+        </Icon>
+      ),
+      value: "rupee",
+    },
+  ],
+});
+
+export default DeleteExpense;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -33,13 +33,27 @@ import {
 } from "../components/ui/select";
 import React from "react";
 import AddExpense from "./AddExpense";
+import EditExpense from "./EditExpense";
+import DeleteExpense from "./DeleteExpense";
 
 const Home = () => {
   const [visible, setVisible] = useState(false);
   const [selection, setSelection] = useState<string[]>([]);
+  const [action, setAction] = useState<string>("add");
 
   const hasSelection = selection.length > 0;
   const indeterminate = hasSelection && selection.length < items.length;
+
+  const renderSwitch = (action: string) => {
+    switch (action) {
+      case "add":
+        return <AddExpense />;
+      case "edit":
+        return <EditExpense />;
+      case "delete":
+        return <DeleteExpense />;
+    }
+  };
 
   const rows = items.map((item) => (
     <Table.Row
@@ -92,6 +106,7 @@ const Home = () => {
             textStyle="lg"
             fontWeight="medium"
             margin="18px 35px 0 0"
+            href="https://github.com/SoftwareEngNoobs/DollarBot"
           >
             About
           </Link>
@@ -100,6 +115,7 @@ const Home = () => {
             textStyle="lg"
             fontWeight="medium"
             margin="18px 35px 0 0"
+            href="https://github.com/SoftwareEngNoobs/DollarBot"
           >
             Help
           </Link>
@@ -111,34 +127,49 @@ const Home = () => {
         axis="both"
         background="black"
       >
-        <Flex overflow="hidden" borderRadius="xl" shadow="lg" padding="20px" divideX="2px" divideColor="#F2F0EF" divideStyle="dashed">
+        <Flex
+          overflow="hidden"
+          borderRadius="xl"
+          shadow="lg"
+          padding="20px"
+          divideX="2px"
+          divideColor="#F2F0EF"
+          divideStyle="dashed"
+        >
           <Flex flexDir="column">
             <Container color="white" background="black">
-                <SelectRoot
-                  collection={frameworks}
-                  size="sm"
-                  width="320px"
-                  variant="subtle"
-                  margin="0 0 15px 0"
-                >
-                  <SelectLabel fontWeight="bold">Action</SelectLabel>
-                  <SelectTrigger>
-                    <SelectValueText color="teal" placeholder="Select Action" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {frameworks.items.map((movie) => (
-                      <SelectItem color="teal" item={movie} key={movie.value}>
-                        {movie.label}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </SelectRoot>
-                {/* <Button variant="solid" background="teal">
-                Update
-                </Button> */}
+              <SelectRoot
+                collection={frameworks}
+                size="sm"
+                width="320px"
+                defaultValue={["add"]}
+                variant="subtle"
+                margin="0 0 15px 0"
+                onValueChange={({ value }) => setAction(value[0])}
+              >
+                <SelectLabel fontWeight="bold">Action</SelectLabel>
+                <SelectTrigger>
+                  <SelectValueText color="teal" placeholder="Select Action" />
+                </SelectTrigger>
+                <SelectContent>
+                  {frameworks.items.map((movie) => (
+                    <SelectItem color="teal" item={movie} key={movie.value}>
+                      {movie.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </SelectRoot>
             </Container>
-            <Box divideY="2px" marginBottom="10px" width="97%" divideColor="#F2F0EF"><Box></Box><Box></Box></Box>
-            <AddExpense />
+            <Box
+              divideY="2px"
+              marginBottom="10px"
+              width="97%"
+              divideColor="#F2F0EF"
+            >
+              <Box></Box>
+              <Box></Box>
+            </Box>
+            {renderSwitch(action)}
           </Flex>
           <Table.Root borderRadius="xl" size="md">
             <Table.Header>
@@ -157,29 +188,20 @@ const Home = () => {
                     }}
                   />
                 </Table.ColumnHeader>
-                <Table.ColumnHeader fontWeight="extrabold">Product</Table.ColumnHeader>
-                <Table.ColumnHeader fontWeight="extrabold">Category</Table.ColumnHeader>
-                <Table.ColumnHeader fontWeight="extrabold">Price</Table.ColumnHeader>
+                <Table.ColumnHeader fontWeight="extrabold">
+                  Product
+                </Table.ColumnHeader>
+                <Table.ColumnHeader fontWeight="extrabold">
+                  Category
+                </Table.ColumnHeader>
+                <Table.ColumnHeader fontWeight="extrabold">
+                  Price
+                </Table.ColumnHeader>
               </Table.Row>
             </Table.Header>
             <Table.Body>{rows}</Table.Body>
           </Table.Root>
         </Flex>
-
-        {/* <ActionBarRoot open={hasSelection}>
-          <ActionBarContent>
-          <ActionBarSelectionTrigger>
-              {selection.length} selected
-            </ActionBarSelectionTrigger>
-            <ActionBarSeparator />
-            <Button variant="outline" size="sm">
-              Delete <Kbd>⌫</Kbd>
-            </Button>
-            <Button variant="outline" size="sm">
-              Share <Kbd>T</Kbd>
-            </Button>
-          </ActionBarContent>
-        </ActionBarRoot> */}
       </AbsoluteCenter>
     </div>
   );
