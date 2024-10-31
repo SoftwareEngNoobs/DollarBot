683852a345cf45291806de4f0cdfd03392d42a3f
Krithika Ragothaman <krithika.ragoth@gmail.com>
2024-10-31 00:23:14 -0400

    Init :  setup sqlalchemy

--- flask_dollarbot/app.py
+++ flask_dollarbot/app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 from endpoints.add import add_bp
 from endpoints.edit import edit_bp
 from endpoints.delete import delete_bp
 from endpoints.display import display_bp
 from endpoints.category import category_bp
+from sqlalchemy import inspect
 # from endpoints.add_recurring import add_recurring_bp
 
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
+
+
+def database_exists():
+    inspector = inspect(db.engine)
+    return len(inspector.get_table_names()) > 0
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+# initialize the app with the extension
+db.init_app(app)
+#create tables if not created already
+with app.app_context():
+    if not database_exists():
+            db.create_all()
   
 # Register blueprints
 app.register_blueprint(add_bp, url_prefix='/add')
